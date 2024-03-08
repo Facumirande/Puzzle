e24518d8045676dcde03eb50f0161952fd7126f1
Facundo <facundomirande96@gmail.com>
2024-03-08 15:19:56 -0300

    changes

--- App/main.js
+++ App/main.js
@@ -2,12 +2,21 @@ const piezas = document.querySelectorAll(".piezasElefante");
 const origX = [200, 304, 446, 200];
 const origY = [100, 100, 233, 204];
 const winAudio = document.getElementById("win");
-const cero = document.querySelector("letraA");
-const uno = document.querySelector("letraB");
-const dos = document.querySelector("letraC");
-const tres = document.querySelector("letraD");
 
-let coordenadasResize = {};
+let letraA = false;
+let letraB = false;
+let letraC = false;
+let letraD = false;
+
+let piezaAOK = false;
+let piezaBOK = false;
+let piezaCOK = false;
+let piezaDOK = false;
+
+let elementoLetraA = document.getElementById("letraA");
+let elementoLetraB = document.getElementById("letraB");
+let elementoLetraC = document.getElementById("letraC");
+let elementoLetraD = document.getElementById("letraD");
 
 piezas.forEach((pieza, index) => {
   const tamWidth = [134, 192, 134, 163];
@@ -29,6 +38,30 @@ let offsetY = 0;
 
 function seleccionarElemento(evt) {
   elementSelect = evt.target;
+  if (elementSelect.id == "letraA") {
+    letraA = true;
+    letraB = false;
+    letraC = false;
+    letraD = false;
+  }
+  if (elementSelect.id == "letraB") {
+    letraA = false;
+    letraB = true;
+    letraC = false;
+    letraD = false;
+  }
+  if (elementSelect.id == "letraC") {
+    letraA = false;
+    letraB = false;
+    letraC = true;
+    letraD = false;
+  }
+  if (elementSelect.id == "letraD") {
+    letraA = false;
+    letraB = false;
+    letraC = false;
+    letraD = true;
+  }
   offsetX = evt.clientX - parseFloat(elementSelect.style.left);
   offsetY = evt.clientY - parseFloat(elementSelect.style.top);
 
@@ -55,14 +88,6 @@ function soltarElemento() {
   testing();
 }
 
-function testing() {
-  const bienUbicadas = Array.from(piezas).filter((pieza, index) => {
-    const posx = parseFloat(pieza.style.left);
-    const posy = parseFloat(pieza.style.top);
-    return origX[index] === posx && origY[index] === posy;
-  });
-}
-
 let miDiv = document.getElementById("mi-div");
 
 let anchoDiv = miDiv.offsetWidth;
@@ -71,7 +96,7 @@ let altoDiv = miDiv.offsetHeight;
 let anchoPorcentual = anchoDiv / 100;
 let altoPorcentual = altoDiv / 100;
 
-function iman(cero, uno, dos, tres) {
+function iman() {
   piezas.forEach((pieza) => {
     pieza.addEventListener("mouseup", () => {
       const posx = parseFloat(pieza.style.left);
@@ -100,39 +125,137 @@ function iman(cero, uno, dos, tres) {
       const distanciaDeteccion = 30;
 
       // Verifica si la pieza se soltó cerca de las coordenadas específicas
-      for (const etiqueta in coordenadas) {
-        if (
-          Math.abs(posx - coordenadas[etiqueta].left) < distanciaDeteccion &&
-          Math.abs(posy - coordenadas[etiqueta].top) < distanciaDeteccion
-        ) {
-          // Mueve la pieza a las coordenadas específicas
-          pieza.style.left = `${coordenadas[etiqueta].left}px`;
-          pieza.style.top = `${coordenadas[etiqueta].top}px`;
+      if (
+        letraA &&
+        Math.abs(posx - coordenadas.A.left) < distanciaDeteccion &&
+        Math.abs(posy - coordenadas.A.top) < distanciaDeteccion
+      ) {
+        // Mueve la pieza a las coordenadas específicas
+        pieza.style.left = `${coordenadas.A.left}px`;
+        pieza.style.top = `${coordenadas.A.top}px`;
 
-          // Desactiva la capacidad de mover la pieza
-          pieza.style.pointerEvents = "none";
+        // Desactiva la capacidad de mover la pieza
+        pieza.style.pointerEvents = "none";
 
-          if ((pieza.style.Events = "none")) {
-            winAudio.play();
-          }
+        piezaAOK = true;
 
-          // Aplica una transición suave para el acomodamiento
-          pieza.style.transition = "top 0.3s, left 0.3s";
-          break; // Detiene el bucle una vez que se encuentra una coincidencia
-        }
+        // Aplica una transición suave para el acomodamiento
+        pieza.style.transition = "top 0.3s, left 0.3s";
       }
-      coordenadasResize = coordenadas;
+
+      if (
+        letraB &&
+        Math.abs(posx - coordenadas.B.left) < distanciaDeteccion &&
+        Math.abs(posy - coordenadas.B.top) < distanciaDeteccion
+      ) {
+        // Mueve la pieza a las coordenadas específicas
+        pieza.style.left = `${coordenadas.B.left}px`;
+        pieza.style.top = `${coordenadas.B.top}px`;
+
+        // Desactiva la capacidad de mover la pieza
+        pieza.style.pointerEvents = "none";
+
+        piezaBOK = true;
+
+        // Aplica una transición suave para el acomodamiento
+        pieza.style.transition = "top 0.3s, left 0.3s";
+      }
+
+      if (
+        letraC &&
+        Math.abs(posx - coordenadas.C.left) < distanciaDeteccion &&
+        Math.abs(posy - coordenadas.C.top) < distanciaDeteccion
+      ) {
+        // Mueve la pieza a las coordenadas específicas
+        pieza.style.left = `${coordenadas.C.left}px`;
+        pieza.style.top = `${coordenadas.C.top}px`;
+
+        // Desactiva la capacidad de mover la pieza
+        pieza.style.pointerEvents = "none";
+
+        piezaCOK = true;
+
+        // Aplica una transición suave para el acomodamiento
+        pieza.style.transition = "top 0.3s, left 0.3s";
+      }
+
+      if (
+        letraD &&
+        Math.abs(posx - coordenadas.D.left) < distanciaDeteccion &&
+        Math.abs(posy - coordenadas.D.top) < distanciaDeteccion
+      ) {
+        // Mueve la pieza a las coordenadas específicas
+        pieza.style.left = `${coordenadas.D.left}px`;
+        pieza.style.top = `${coordenadas.D.top}px`;
+
+        // Desactiva la capacidad de mover la pieza
+        pieza.style.pointerEvents = "none";
+
+        piezaDOK = true;
+
+        // Aplica una transición suave para el acomodamiento
+        pieza.style.transition = "top 0.3s, left 0.3s";
+      }
     });
   });
 }
 
-
-const coordenadaLeft = miDiv.offsetLeft;
-const coordenadaTop = miDiv.offsetTop;
-
+let coordenadaLeft = miDiv.offsetLeft;
+let coordenadaTop = miDiv.offsetTop;
 
 window.onresize = resize;
 
 function resize() {
-  console.log(coordenadasResize);
+  miDiv = document.getElementById("mi-div");
+  anchoDiv = miDiv.offsetWidth;
+  altoDiv = miDiv.offsetHeight;
+  coordenadaLeft = miDiv.offsetLeft;
+  coordenadaTop = miDiv.offsetTop;
+  if (piezaAOK) {
+    elementoLetraA.style.left = `${coordenadaLeft - (anchoDiv / 100) * 4.5}px`;
+    elementoLetraA.style.top = `${coordenadaTop - (altoDiv / 100) * 8.3}px`;
+  }
+  if (piezaBOK) {
+    elementoLetraB.style.left = `${coordenadaLeft - (anchoDiv / 100) * -37}px`;
+    elementoLetraB.style.top = `${coordenadaTop - (altoDiv / 100) * 1.5}px`;
+  }
+  if (piezaCOK) {
+    elementoLetraC.style.left = `${coordenadaLeft - (anchoDiv / 100) * 8.5}px`;
+    elementoLetraC.style.top = `${coordenadaTop - (altoDiv / 100) * 100}px`;
+  }
+  if (piezaDOK) {
+    elementoLetraD.style.left = `${
+      coordenadaLeft - (anchoDiv / 100) * -31.5
+    }px`;
+    elementoLetraD.style.top = `${coordenadaTop - (altoDiv / 100) * 38.7}px`;
+  }
+}
+
+// const coordenadas = {
+//   A: {
+//     left: coordenadaLeft - anchoPorcentual * 4.5,
+//     top: coordenadaTop - altoPorcentual * 8.3,
+//   },
+//   B: {
+//     left: coordenadaLeft + anchoPorcentual * 37.1,
+//     top: coordenadaTop - altoPorcentual * 1.5,
+//   },
+//   C: {
+//     left: coordenadaLeft - anchoPorcentual * 8.5,
+//     top: coordenadaTop + altoPorcentual * 31.8,
+//   },
+//   D: {
+//     left: coordenadaLeft + anchoPorcentual * 31.4,
+//     top: coordenadaTop + altoPorcentual * 38.7,
+//   },
+// };
+
+function testing() {
+  // Obtén la referencia al elemento del switch
+  const switchElement = document.getElementById("Activar");
+
+  // Verifica si el switch está activado
+  if (switchElement.checked && piezaAOK && piezaBOK && piezaCOK && piezaDOK) {
+    winAudio.play();
+  }
 }
